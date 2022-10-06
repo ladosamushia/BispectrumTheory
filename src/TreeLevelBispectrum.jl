f88bd3e04937724dd3ac650840f16e5d9187f4fc
Lado <lado.samushia.office@gmail.com>
2022-10-05 21:51:58 -0500

    triangular geometry tested and works. need to finish testing the bispectrum.

--- src/TreeLevelBispectrum.jl
+++ src/TreeLevelBispectrum.jl
@@ -6,13 +6,34 @@
 using Interpolations
 
 """
+    solve_triangular_geometry(μ1, ϕ, k1, k2, k3)
+
+    Find all relevant angles of the triangle. μ_i is the cosine of the angle 
+    of the ki vector with respect to the z-axis. μij is the cosine of the
+    angle between vectors ki and kj.
+
+    Input:
+    -μ1, ϕ, k1, k2, k3
+    Output:
+    -μ2, μ3, μ12, μ23, μ31
+"""
+function solve_triangular_geometry(μ1, ϕ, k1, k2, k3)
+    μ12 = (k1^2 + k2^2 - k3^2)/(2*k1*k2)
+    μ31 = (k3^2 + k1^2 - k2^2)/(2*k3*k1)
+    μ23 = (k2^2 + k3^2 - k1^2)/(2*k2*k3)
+    μ3 = - μ1*μ12 - sqrt(1 - μ1^2)*sqrt(1 - μ12^2)*cos(ϕ)
+    μ2 = -(μ1*k1 + μ3*k3)/k2
+    return μ2, μ3, μ12, μ23, μ31
+end
+
+"""
     tree_level_bk(μ1, ϕ, k1, k2, k3, b1, b2, f)
 
     compute tree level bispectrum in 3D
 
     -Input:
     -μ1, ϕ::Float64: angles describing bispectrum orientation (see. Gagrani &
-     Samushia)
+     Samushia). ϕ in radians.
     - k1, k2, k3::Array{Float64,1}: wavenumbers describing the triangle shape
     - b1, b2, f::Float64: bias parameters and the growth rete. 
     - pk::Interpolations.GriddedInterpolation: interpolator for the power
@@ -27,11 +48,7 @@ function tree_level_bk(μ1, ϕ, k1, k2, k3, b1, b2, f, pk)
     pk2 = pk(k2)
     pk3 = pk(k3)
 
-    μ12 = -(k3^2 - k1^2 - k2^2)/(2*k1*k2)
-    μ2 = μ1*μ12 - sqrt(1 - μ1^2)*sqrt(1 - μ12^2)*cos(ϕ)
-    μ3 = -(μ1*k1 + μ2*k2)/k3
-    μ31 = -(k2^2 - k1^2 - k3^2)/(2*k1*k3)
-    μ23 = -(k1^2 - k3^2 - k2^2)/(2*k3*k2)
+    μ2, μ3, μ12, μ23, μ31 = solve_triangular_geometry(μ1, ϕ, k1, k2, k3)
  
     Z1k1 = b1 + f*μ1^2
     Z1k2 = b1 + f*μ2^2
--- test/TreeLevelBispectrum_test.jl
+++ test/TreeLevelBispectrum_test.jl
@@ -7,15 +7,40 @@ using Test
 
 include("../src/TreeLevelBispectrum.jl")
 
+@testset "solve_triangular_geometry" begin
+    computation = solve_triangular_geometry(0,0,1,1,1)
+    expectation = [sqrt(3)/2, -sqrt(3)/2, 1/2, 1/2, 1/2]
+    @test all(abs.(computation .- expectation) .< 1e-6)
+    computation = solve_triangular_geometry(1,0,1,1,1)
+    expectation = [-1/2, -1/2, 1/2, 1/2, 1/2]
+    @test all(abs.(computation .- expectation) .< 1e-6)
+    computation = solve_triangular_geometry(sqrt(3)/2,0,1,1,1) 
+    expectation = [0, -sqrt(3)/2, 1/2, 1/2, 1/2]
+    @test all(abs.(computation .- expectation) .< 1e-6)
+    computation = solve_triangular_geometry(1/2,0,1,1,1)
+    expectation = [1/2, -1, 1/2, 1/2, 1/2]
+    @test all(abs.(computation .- expectation) .< 1e-6)
+    computation = solve_triangular_geometry(1,pi/3,1,1,1)
+    expectation = [-1/2, -1/2, 1/2, 1/2, 1/2]
+    @test all(abs.(computation .- expectation) .< 1e-6)
+    computation = solve_triangular_geometry(0,pi/2,1,1,1)
+    expectation = [0, 0, 1/2, 1/2, 1/2]
+    @test all(abs.(computation .- expectation) .< 1e-6)
+end
+
 # Do some limiting values that are easy to compute by hand
-#@testset "TreeLevelBispectrum" begin
+@testset "TreeLevelBispectrum" begin
     pk = interpolate(([0, 1, 2, 3],), [1, 1, 1, 1], Gridded(Linear()))
 
-    #μ1 = 0; ϕ = 0; k1 = 1.0; k2 = 1.0; k3 = 1.0; b1 = 1; b2 = 1; f = 0
-    #bk = tree_level_bk(μ1, ϕ, k1, k2, k3, b1, b2, f, pk)
+    μ1 = 0; ϕ = 0; k1 = 1.0; k2 = 1.0; k3 = 1.0; b1 = 1; b2 = 1; f = 0
+    bk = tree_level_bk(μ1, ϕ, k1, k2, k3, b1, b2, f, pk)
     #@test isapprox(bk, 3*2*25/14)
 
-    μ1 = 0; ϕ = 0; k1 = 1.0; k2 = 1.0; k3 = 1.0; b1 = 0; b2 = 0; f = 1
+    μ1 = 1/2; ϕ = 0; k1 = 1.0; k2 = 1.0; k3 = 1.0; b1 = 1; b2 = 0; f = 1
     bk = tree_level_bk(μ1, ϕ, k1, k2, k3, b1, b2, f, pk)
-    #@test isapprox(bk, 1/28)
-#end;
+    #@test isapprox(bk, 2*63/16)
+
+    μ1 = 1; ϕ = pi/2; k1 = 1.0; k2 = 1.0; k3 = 1.0; b1 = 1; b2 = 0; f = 1
+    bk = tree_level_bk(μ1, ϕ, k1, k2, k3, b1, b2, f, pk)
+    #@test isapprox(bk, 2*201/16)
+end;
